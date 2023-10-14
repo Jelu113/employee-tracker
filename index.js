64f87940aede5a158ea94b210a1523223f965317
jelu <jenaehe@yahoo.com>
2023-10-14 17:22:10 -0600

    got the updates working

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const mainMenu = () => {
                     'Add Department',
                     'Add Role',
                     'Add Employee',
+                    'Update Departments',
+                    'Update Roles',
+                    'Update Employees',
                 ],
             },
 
@@ -57,8 +60,16 @@ const mainMenu = () => {
                 case 'Add Employee':
                     addEmployee();
                     break;
+                case 'Update Departments':
+                    updateDepartment();
+                    break;
+                case 'Update Roles':
+                    updateRole();
+                    break;
+                case 'Update Employees':
+                    updateEmployee();
+                    break;
             }
-
         });
 };
 
@@ -127,7 +138,7 @@ const addRole = () => {
     inquirer
         .prompt([
             {
-                type: 'input', 
+                type: 'input',
                 message: 'Enter the title of the role: ',
                 name: 'roleTitle',
             },
@@ -157,41 +168,143 @@ const addRole = () => {
 
 const addEmployee = () => {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter the Employees first name:',
-            name: 'firstName',
-        },
-        {
-            type: 'input',
-            message: 'Enter the Employees last name:',
-            name: 'lastName',
-        },
-        {
-            type: 'input',
-            message: 'Enter the employee role ID (foreign key):',
-            name: 'roleID',
-        },
-        {
-            type: 'input',
-            message: 'Enter the Employee manager ID:',
-            name: 'managerID',
-        },
-    ])
-    .then((data) => {
-        const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
-        db.query(sql, [data.firstName, data.lastName, data.roleID, data.managerID], (err, result) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Employee added successfully!');
-            }
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter the Employees first name:',
+                name: 'firstName',
+            },
+            {
+                type: 'input',
+                message: 'Enter the Employees last name:',
+                name: 'lastName',
+            },
+            {
+                type: 'input',
+                message: 'Enter the employee role ID (foreign key):',
+                name: 'roleID',
+            },
+            {
+                type: 'input',
+                message: 'Enter the Employee manager ID:',
+                name: 'managerID',
+            },
+        ])
+        .then((data) => {
+            const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+            db.query(sql, [data.firstName, data.lastName, data.roleID, data.managerID], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Employee added successfully!');
+                }
+            });
         });
-    });
 
 }
 
+const updateDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter the department ID you want to update:',
+                name: 'departmentID',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new department name:',
+                name: 'newDepartmentName',
+            },
+        ])
+        .then((data) => {
+            const sql = 'UPDATE department SET name = ? WHERE id = ?';
+            db.query(sql, [data.newDepartmentName, data.departmentID], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Department updated successfully!');
+                }
+            });
+        });
+};
+
+const updateRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter the role ID you want to update:',
+                name: 'roleID',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new title for the role:',
+                name: 'newRoleTitle',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new salary for the role:',
+                name: 'newRoleSalary',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new department ID (foreign key):',
+                name: 'newDepartmentID',
+            },
+        ])
+        .then((data) => {
+            const sql = 'UPDATE role SET title = ?, salary = ?, department_id = ? WHERE id = ?';
+            db.query(sql, [data.newRoleTitle, data.newRoleSalary, data.newDepartmentID, data.roleID], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Role updated successfully!');
+                }
+            });
+        });
+};
+
+const updateEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter the employee ID you want to update:',
+                name: 'employeeID',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new first name for the employee:',
+                name: 'newFirstName',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new last name for the employee:',
+                name: 'newLastName',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new role ID (foreign key) for the employee:',
+                name: 'newRoleID',
+            },
+            {
+                type: 'input',
+                message: 'Enter the new manager ID for the employee:',
+                name: 'newManagerID',
+            },
+        ])
+        .then((data) => {
+            const sql = 'UPDATE employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ? WHERE id = ?';
+            db.query(sql, [data.newFirstName, data.newLastName, data.newRoleID, data.newManagerID, data.employeeID], (err, result) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Employee updated successfully!');
+                }
+            });
+        });
+};
 
 mainMenu();
 
